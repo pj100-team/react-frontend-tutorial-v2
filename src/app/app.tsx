c01c79d6fd349f86f0c99c4eb9ba1d30540386fd
野村萌華 <nomura@aifurukaihatsu002noMacBook-Pro.local>
2022-03-03 13:13:03 +0900

    finish

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 
+const queryClient = new QueryClient()
+
 const App: React.FC = () => (
   <>
+  <QueryClientProvider client={queryClient}>
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
+    </QueryClientProvider>
   </>
 );
 
--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import Caption from "../components/common/caption";
-import ListTile from "../components/common/listTile";
-import PlainText from "../components/common/plainText";
-import Title from "../components/common/title";
+import AddressSearch from "../components/common/addressSearch";
 import PageWrapper from "../components/layout/pageWrapper";
 import { BackgroundColor } from "../view/helper/layoutHelper";
 
 const Home: React.FC = () => (
   <PageWrapper color={BackgroundColor.SubBackground}>
-    <Title>React Tutorial</Title>
-    <Caption>Aiful</Caption>
-    <PlainText>text</PlainText>
-    <ListTile title="title" subtitle="subtitle"/>
+    <AddressSearch/>
   </PageWrapper>
 );
 
