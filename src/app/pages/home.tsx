de7345d40449424c6c7bad6a55fd4c35b2cd2846
長岡竜太 <nagaokaryuta@aifurukaihatsu003noMacBook-Pro.local>
2022-12-23 13:07:32 +0900

    finish#2

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -5,12 +5,14 @@ import PageWrapper from "../components/layout/pageWrapper";
 import { BackgroundColor } from "../view/helper/layoutHelper";
 import PlainText from "../components/common/plainText";
 import { FontColor } from "../view/helper/typographyHelper";
+import CheckBox from "../components/common/checkBox";
 
 const Home: React.FC = () => (
   <PageWrapper color={BackgroundColor.SubBackground}>
     <Title>React Tutorial</Title>
     <Caption>Aiful</Caption>
     <PlainText fontColor={FontColor.SubHeadline}>あああああああ</PlainText>
+    <CheckBox />
   </PageWrapper>
 );
 
