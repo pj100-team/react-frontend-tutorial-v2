4404638b14de98c85eee45d68dabf5e697b1b5af
Steki Chung <chung@AIFUL005noMacBook-Pro.local>
2023-02-13 16:07:48 +0900

    Complete Issue 2

--- src/app/components/common/checkBox.tsx
+++ src/app/components/common/checkBox.tsx
@@ -1,33 +1,27 @@
 // useState is a Hook that lets you add React state to function components
-import { useState } from 'react';
+// import React, { useState } from 'react';
+import React from "react";
 
-// function toggle(value: boolean){
-//     return !value;
-//   }
+interface Props {
+  checked: boolean;
+  setChecked: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-const CheckBoxApp = () => {
-  // Use React's useState Hook
-  const [checked, setChecked] = useState(false);
-
-  // Use an event handler
-  const handleChange = () => {
-    setChecked(!checked);
-  };
-
+// the checkbox component
+const Checkbox: React.FC<Props> = ({
+  checked, // parameters
+  setChecked
+}) => {
+    // Use an event handler
+    const handleChange = () => {setChecked(!checked)};
+  
   return (
-    <div>
-      <label>
         <input
           type="checkbox"
           checked={checked}
           onChange={handleChange}
         />
-        Label
-      </label>
-    </div>
   )
 }
 
-const Checkbox = (props) =>
-
-export default CheckBoxApp;
+export default Checkbox;
--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -5,15 +5,25 @@ import PlainText from "../components/common/plainText";
 import Title from "../components/common/title";
 import PageWrapper from "../components/layout/pageWrapper";
 import { BackgroundColor } from "../view/helper/layoutHelper";
+import { FontColor } from "../view/helper/typographyHelper";
 
-const Home: React.FC = () => (
+const Home: React.FC = () => {
+
+  // initialise parameters for the checkboxes
+  const [checked1, setChecked1] = React.useState(false);
+  const [checked2, setChecked2] = React.useState(false);
+
+  return (
+
   <PageWrapper color={BackgroundColor.SubBackground}>
     <Title>React Tutorial</Title>
     <Caption>Aiful</Caption>
     <PlainText>Plain text</PlainText>
-    <PlainText>Plain text</PlainText>
-    <CheckBox>Label</CheckBox>
+    <PlainText fontColor={FontColor.Error}>Plain text</PlainText>
+    <CheckBox checked={checked1} setChecked={setChecked1}></CheckBox>
+    <CheckBox checked={checked2} setChecked={setChecked2}></CheckBox>
   </PageWrapper>
 );
+}
 
 export default Home;
