83c762bc35f6485047662d8868b4544f23de7dd4
Steki Chung <chung@AIFUL005noMacBook-Pro.local>
2023-02-13 17:16:26 +0900

    Complete Issue 3 but alignment incorrect

--- src/app/components/common/listTile.tsx
+++ src/app/components/common/listTile.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import CheckBox from "../common/checkBox";
+import Title from "../common/title";
+import Caption from "../common/caption";
 
 interface Props {
     title: string;
     subtitle: string;
+    checked: boolean;
+    setChecked: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const ListTile: React.FC<Props> = ({
@@ -16,11 +21,18 @@ const ListTile: React.FC<Props> = ({
         <div>
             <div
                 style={{
-                    backgroundColor: isChecked ? 'blue' : '', 
+                    border: 'solid 2px #329eff',
+                    borderRadius: '5px',
+                    width: '200px',
+                    backgroundColor: checked ? 'salmon' : '',
+                    overflowWrap: 'break-word'
                 }}>
-
+                <Title>{title}</Title>
+                <p style={{ textAlign: "right" }}><CheckBox checked={checked} setChecked={setChecked}></CheckBox></p>
+                <Caption>{subtitle}</Caption>
             </div>
         </div>
     )
-
 }
+
+export default ListTile;
