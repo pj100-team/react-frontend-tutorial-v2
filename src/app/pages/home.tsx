199a00490a95a72dfdb61b145548e3bb31b7dbd3
Steki Chung <chung@AIFUL005noMacBook-Pro.local>
2023-02-16 15:20:28 +0900

    Complete Issue 4 and Modified listTile.tsx

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Caption from "../components/common/caption";
 import CheckBox from "../components/common/checkBox";
+import CheckList from "../components/common/checkList";
 import ListTile from "../components/common/listTile";
 import PlainText from "../components/common/plainText";
 import Title from "../components/common/title";
@@ -13,7 +14,8 @@ const Home: React.FC = () => {
   // initialise parameters for the checkboxes
   const [checked1, setChecked1] = React.useState(false);
   const [checked2, setChecked2] = React.useState(false);
-  const [checkedListTile, setCheckedListTile] = React.useState(false);
+  // list tile
+  const initialCheckListTile = false;
 
   return (
 
@@ -24,7 +26,11 @@ const Home: React.FC = () => {
     <PlainText fontColor={FontColor.Error}>Plain text</PlainText>
     <CheckBox checked={checked1} setChecked={setChecked1}></CheckBox>
     <CheckBox checked={checked2} setChecked={setChecked2}></CheckBox>
-    <ListTile title="List title" subtitle="Very Very Very Very Very Long List subtitle" checked={checkedListTile} setChecked={setCheckedListTile}></ListTile>
+    <ListTile title="List title"
+              subtitle="Very Very Very Very Very Long List subtitle"
+              initialCheck={initialCheckListTile} />
+    <br></br>
+    <CheckList width="250px"></CheckList>
   </PageWrapper>
 );
 }
