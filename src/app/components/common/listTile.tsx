ae5336f99e7b61c884a12c306ebd11e16f553237
Steki Chung <chung@AIFUL005noMacBook-Pro.local>
2023-02-16 13:02:33 +0900

    Complete Issue 3

--- src/app/components/common/listTile.tsx
+++ src/app/components/common/listTile.tsx
@@ -2,9 +2,10 @@
 import React from "react";
 import { css } from "@emotion/react";
 import CheckBox from "../common/checkBox";
-import Title from "../common/title";
-import Caption from "../common/caption";
+import ListTileTitle from "../common/listTileTitle";
 import { ElementColor } from "../../view/helper/layoutHelper";
+import PlainText from "./plainText";
+import { FontColor } from "../../view/helper/typographyHelper";
 
 interface Props {
     title: string;
@@ -23,15 +24,23 @@ const ListTile: React.FC<Props> = ({
     return (
         <div
         css={css`
-                    border: solid 2px #329eff;
+                    border: solid 2px ${ElementColor.Main};
                     border-radius: 5px;
                     width: 200px;
-                    background-color: ${checked ? ElementColor.Sub : ElementColor.Main};
+                    background-color: ${checked ? ElementColor.White : ElementColor.Sub};
                     overflow-wrap: break-word;
             `}>
-            <div css={css`margin-right: 100px;`}><Title>{title}</Title></div>
-            <p css={css`text-align: right; `}><CheckBox checked={checked} setChecked={setChecked}></CheckBox></p>
-            <div css={css`margin-right: 100px;`}><Caption>{subtitle}</Caption></div>
+            <div css={css`
+                            margin-left: 10px;
+                            margin-right: 40px;
+                            margin-top: 10px;
+                `}><ListTileTitle>{title}</ListTileTitle></div>
+            <p css={css`text-align: right; margin-right: 10px;`}><CheckBox checked={checked} setChecked={setChecked}></CheckBox></p>
+            <div css={css`
+                            margin-left: 10px;
+                            margin-right: 40px;
+                            margin-bottom: 10px;
+                `}><PlainText>{subtitle}</PlainText></div>
         </div>
     )
 }
