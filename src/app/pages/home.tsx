677ba8c39706f7a4014d028ce602b5341e0974a1
holarora <liu.lijia.82v@st.kyoto-u.ac.jp>
2023-08-28 15:19:11 +0900

    #6

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -3,18 +3,13 @@ import Caption from "../components/common/caption";
 import Title from "../components/common/title";
 import PageWrapper from "../components/layout/pageWrapper";
 import { BackgroundColor } from "../view/helper/layoutHelper";
-import Plaintext from "../components/common/plaintext";
-import { FontColor } from "../view/helper/typographyHelper";
-import ListTile from "../components/common/listtile";
-import CheckList from "../components/common/checklist";
+import AddressSearch from "../components/common/addresssearch";
 
 const Home: React.FC = () => (
   <PageWrapper color={BackgroundColor.SubBackground}>
     <Title>React Tutorial</Title>
     <Caption>Aiful</Caption>
-    <Plaintext fontColor={FontColor.Paragraph}>hi</Plaintext>
-    <ListTile title="I am title" subtitle="I am subtitle" initialChecked={true} />
-    <CheckList width="100%"/>
+    <AddressSearch/>
   </PageWrapper>
 );
 
