ea5276dfdade4dbedbb0ba42d18f08138de9b6ec
holarora <liu.lijia.82v@st.kyoto-u.ac.jp>
2023-08-30 11:45:38 +0900

    #6 after modification

--- src/app/components/common/address_search/addresssearch.tsx
+++ src/app/components/common/address_search/addresssearch.tsx
@@ -3,6 +3,17 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./addresssearch.css"
 
+interface AddressInformation {
+    address1:"",
+    address2:"",
+    address3:"",
+    kana1:"",
+    kana2:"",
+    kana3:"",
+    prefcode:"",
+    zipcode:"",
+}
+
 const AddressSearch = () => {
     const [text, setText] = useState("");
     const [address, setAddress] = useState({
@@ -13,12 +24,24 @@ const AddressSearch = () => {
         kana2:"",
         kana3:"",
         prefcode:"",
-        zipcode:""
+        zipcode:"",
     });
     const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     const onClickFetchData = async () => {
+        let updateAddressInformation = {
+            address1:"",
+            address2:"",
+            address3:"",
+            kana1:"",
+            kana2:"",
+            kana3:"",
+            prefcode:"",
+            zipcode:"",
+        };
         setIsLoading(true);
+        setIsError(false);
         await axios
           .get("https://zipcloud.ibsnet.co.jp/api/search", {
             params: {
@@ -26,54 +49,55 @@ const AddressSearch = () => {
             },
           })
           .then((res) => {
-            setAddress({
-                address1: res.data.results[0].address1,
-                address2: res.data.results[0].address2,
-                address3: res.data.results[0].address3,
-                kana1: res.data.results[0].kana1,
-                kana2: res.data.results[0].kana2,
-                kana3: res.data.results[0].kana3,
-                prefcode: res.data.results[0].prefcode,
-                zipcode: res.data.results[0].zipcode,
+            const address: AddressInformation[] = res.data.results;
+            address.map((item: AddressInformation) => {
+                for (const key in item) {
+                    updateAddressInformation[key as keyof AddressInformation] =
+                        item[key as keyof AddressInformation];
+                }
             });
             setIsLoading(false);
+            setAddress(updateAddressInformation)
           })
-          .catch((error) => {
-            alert(error.message);
+          .catch(() => {
+            setIsError(true);
             setIsLoading(false);
-          })
-    }
+          });
+    };
 
     return(
         <div>
             <div className="title">Address Search</div>
-            <label>
-                <input
-                    type = "text"
-                    value={text}
-                    pattern="[0-9]{7}"
-                    onChange={(event) => setText(event.target.value)}
-                />
-                <span className="error-message">
-                    正しい郵便番号で入力してください。
-                </span>
-            </label>
+            <input
+                type = "text"
+                value={text}
+                pattern="[0-9]{7}"
+                onChange={(event) => setText(event.target.value)}
+            />
             <button onClick={onClickFetchData}>検索</button>
-            <div>{isLoading ? <p>Loading...</p> : null}</div>
-            <div className="results">
-                <p>都道府県：{address.address1}</p>
-                <p>市区町村：{address.address2}</p>
-                <p>町域：{address.address3}</p>
-                <p>都道府県カナ：{address.kana1}</p>
-                <p>市区町村カナ：{address.kana2}</p>
-                <p>町域カナ：{address.kana3}</p>
-                <p>都道府県コード：{address.address1}</p>
-                <p>郵便番号：{address.zipcode}</p>
-            </div>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    {!isError ? (
+                        <div className="results">
+                            <p>都道府県：{address.address1}</p>
+                            <p>市区町村：{address.address2}</p>
+                            <p>町域：{address.address3}</p>
+                            <p>都道府県カナ：{address.kana1}</p>
+                            <p>市区町村カナ：{address.kana2}</p>
+                            <p>町域カナ：{address.kana3}</p>
+                            <p>都道府県コード：{address.address1}</p>
+                            <p>郵便番号：{address.zipcode}</p>
+                        </div>
+                    ) : (
+                        <p className="error-message">正しい郵便番号を入力してください</p>
+                    )}
+                </>
+            )}
         </div>
 
     );
-
 }
 
 
